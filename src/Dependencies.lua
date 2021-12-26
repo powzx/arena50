744d56bf3b9b9343b6b214b62225202fdaf33b4b
powzx <69313768+powzx@users.noreply.github.com>
2021-12-26 15:31:46 +0800

    Initial commit with skeletal code

---
+++ src/Dependencies.lua
@@ -0,0 +1,25 @@
+Class = require 'lib/class'
+push = require 'lib/push'
+Timer = require 'lib/knife.timer'
+
+require 'src/constants'
+require 'src/StateMachine'
+
+require 'src/states/BaseState'
+require 'src/states/game/PlayState'
+
+gSounds = {
+
+}
+
+gTextures = {
+
+}
+
+gFrames = {
+
+}
+
+gFonts = {
+	
+}

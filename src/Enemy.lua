1cb1fc1d072e419891dd2e1357944256f2ec0351
powzx <69313768+powzx@users.noreply.github.com>
2021-12-28 01:17:39 +0800

    Implement render offsets for different enemy states

--- src/Enemy.lua
+++ src/Enemy.lua
@@ -36,11 +36,25 @@ function Enemy:update(dt)
 end
 
 function Enemy:render()
+	self:calculateRenderOffset()
 	love.graphics.draw(gTextures['enemy-' .. self.stateMachine.name], gFrames['enemy-' .. self.stateMachine.name][self.currentAnimation:getCurrentFrame()],
-        math.floor(self.x) + ENEMY_WIDTH / 2, math.floor(self.y) + ENEMY_HEIGHT / 2, 0, 
+        math.floor(self.x) + ENEMY_WIDTH / 2 + self.offsetX, math.floor(self.y) + ENEMY_HEIGHT / 2 + self.offsetY, 0, 
         self.direction == 'right' and 1 or -1, 1, ENEMY_WIDTH / 2, ENEMY_HEIGHT / 2)
 	--self.hurtbox:render()
 	--if self.hitbox.isEffective then
 	--	self.hitbox:render()
 	--end
+end
+
+function Enemy:calculateRenderOffset()
+	if self.stateMachine.name == 'attack' then
+		self.offsetX = 2
+		self.offsetY = -5
+	elseif self.stateMachine.name == 'hit' then
+		self.offsetX = 5
+		self.offsetY = 0
+	else
+		self.offsetX = 0
+		self.offsetY = 0
+	end
 end
